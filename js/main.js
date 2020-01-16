2c103066717f56626c8088b30c79dae08f6007fc
wml <wml@meitu.com>
2020-01-16 15:19:07 +0800

    ！*★,°*:.☆(￣▽￣)/$:*.°★* 。

---
+++ js/main.js
@@ -0,0 +1,3 @@
+(function () {
+  console.log('%cCoded By Ming', 'background-image:-webkit-gradient(linear, left top, right top, color-stop(0, #fac),color-stop(1, #abc) );color:transparent;-webkit-background-clip: text;font-size:12px;color:white;padding:2px 5px')
+})()
